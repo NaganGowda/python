dd48650d18db5f6c60dc683ea20453e86e748f5a
NaganGowda <118706849+NaganGowda@users.noreply.github.com>
2023-05-02 22:37:19 +0530

    d

---
+++ 1.py
@@ -0,0 +1,26 @@
+# string repetition
+a = input() + " "
+print(a * 3)
+
+# simple square
+print("**")
+print("**")
+
+# or
+
+print("* *")
+print("* *")
+
+# word 
+
+word = "Nagan"
+print("* * * " + word + " * * *")
+
+#get first and last char
+n = "Nagan"
+print(n[0])
+print(n[-1])
+
+#reverse the digits
+a = "Nagan"
+print(a[::-1)
---
+++ demo.py
@@ -0,0 +1,8 @@
+a = 1 
+
+
+for i in range(1, 10):
+  a *= i 
+
+print(a)  
+
